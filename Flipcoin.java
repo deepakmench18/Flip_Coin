5f27964f511210bbd2330775c433c7e717a14456
deepakmench18 <deepakmench18@gmail.com>
2021-06-23 15:07:21 +0530

    Utility Program

--- Flipcoin.java
+++ Flipcoin.java
@@ -1,12 +1,33 @@
-package com.bridgelabz.programs;
-import com.bridgelabz.utility.Utility;
-public class Flipcoin {
+package com.bridgelabz.utility;
+import java.util.Scanner;
+public class Utility {
+	Scanner scanner;
 
-	public static void main(String[] args) {
-			Utility utility = new Utility();
-			System.out.println("Enter number of time to flip");
-			int numberOfFlip = utility.getIntValue();
-			utility.flipCoin(numberOfFlip);
-		}
+	 public Utility() {
+		 scanner = new Scanner(System.in);
+	 }
+	 public int getIntValue() {
+		 return scanner.nextInt();
+	 }
+	 // method to return flip coin percentile
+	 public void flipCoin(int numberOfFlip) {
+		 double heads =0,tails = 0;
+			for(int i = 0;i < numberOfFlip; i++) {
+			double side = Math.random();
+			if(side>0.5) {
+				heads++;
+			}
+			else {
+				tails++;
+				}
+			}
+			System.out.println("Number of head :" + heads);
+			System.out.println("Number of tail :" + (numberOfFlip-heads));
+			double headPercent = ((heads * 100)/numberOfFlip);
+			double tailPercent = ((tails * 100)/numberOfFlip);
+			System.out.println("Heads Percentage :" + headPercent + "%");
+			System.out.println("Tails Percentage :" + tailPercent + "%");
+			}
 
-	}
+}
+
